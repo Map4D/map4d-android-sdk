fa0ad5face681969061f80ea25df54440966ab67
Sua Le <sua8051@gmail.com>
2019-01-15 17:12:28 +0700

    - release version 1.1.0

--- simplemap/app/src/main/java/vn/map4d/simplemap/MainActivity.kt
+++ simplemap/app/src/main/java/vn/map4d/simplemap/MainActivity.kt
@@ -2,7 +2,6 @@ package vn.map4d.simplemap
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 import vn.map4d.map4dsdk.maps.Map4D
 import vn.map4d.map4dsdk.maps.OnMapReadyCallback
 
@@ -11,10 +10,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mapView.getMapAsync(this)
+
     }
 
     override fun onMapReady(map4D: Map4D?) {
-        map4D?.enable3DMode(false)
+        map4D?.enable3DMode(true)
+        map4D.o
     }
 }
